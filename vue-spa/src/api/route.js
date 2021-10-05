bd9e8f2aa9ca881c7f1beeffc0ac7f110764b316
Mark <69708300+Mark-vas@users.noreply.github.com>
2021-10-05 16:36:17 +0300

    Delete vue-spa directory

--- vue-spa/src/api/route.js
+++
@@ -1,4 +0,0 @@
-export const CHARACTERS_PAGE = (page) => `character/?page=${page}`
-export const CHARACTER_ID = (id) => `character/${id}`
-export const EPISODE_PAGE = (page) => `episode/?page=${page}`
-export const EPISODE_ID = (id) => `episode/${id}`
