2c2c5ceb1ab427ef3cfead4ddf3436f71363cadf
Mark-vas <vasyaev.mark@mail.ru>
2021-10-05 17:57:05 +0300

    еще одна попытка

--- vue-spa/webpack.config.js
+++ vue-spa/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: 'https://mark-vas.github.io/Vue-test-task/vue-spa/dist/',
+    publicPath: '/dist/',
     filename: 'build.js'
   },
   module: {
